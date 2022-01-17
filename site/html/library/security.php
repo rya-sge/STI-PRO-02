11dbbc7f24be490e7eff81e18dbf5f5c1502f67a
rya-sge <ryan.sauge@heig-vd.ch>
2022-01-17 21:20:38 +0100

    fix bugs csrf

--- site/html/library/security.php
+++ site/html/library/security.php
@@ -1,4 +1,24 @@
 <?php
+if(!function_exists('hash_equals'))
+{
+    function hash_equals($str1, $str2)
+    {
+        if(strlen($str1) != strlen($str2))
+        {
+            return false;
+        }
+        else
+        {
+            $res = $str1 ^ $str2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--)
+            {
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
+    }
+}
 /**
  * verify the CSRF token
  * @throws Exception
@@ -7,26 +27,7 @@
 function verifCSRF(){
     $isValid = false;
     if (!empty($_POST['token'])) {
-        if(!function_exists('hash_equals'))
-        {
-            function hash_equals($str1, $str2)
-            {
-                if(strlen($str1) != strlen($str2))
-                {
-                    return false;
-                }
-                else
-                {
-                    $res = $str1 ^ $str2;
-                    $ret = 0;
-                    for($i = strlen($res) - 1; $i >= 0; $i--)
-                    {
-                        $ret |= ord($res[$i]);
-                    }
-                    return !$ret;
-                }
-            }
-        }else if (hash_equals($_SESSION['token'], $_POST['token'])) {
+        if (hash_equals($_SESSION['token'], $_POST['token'])) {
             $isValid = true;
         }
     }
